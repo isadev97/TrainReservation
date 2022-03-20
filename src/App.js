3207362924d1827954c49cda78c017ac5b982c75
isadev97 <isa325697@gmail.com>
2022-03-20 20:45:21 +0530

    added booked tickets ui msg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import UserInput from "./components/UserInput";
 import StatusBar from "./components/StatusBar";
 import SeatMatrix from "./components/SeatMatrix";
-import SeatNumbers from "./components/SeatNumbers";
+import SeatsBooked from "./components/SeatsBooked";
 import cogoToast from "cogo-toast";
 import {
   generateTrainRows,
@@ -30,6 +30,8 @@ function App() {
   const [reservedSeats, setReservedSeats] = useState(reservedSeatsCount);
   const [vacantSeats, setVacantSeats] = useState(vacantSeatCount);
   const [userInput, setUserInput] = useState(1);
+  const [isBooked, setIsBooked] = useState(false);
+  const [seatsBooked, setSeatsBooked] = useState("");
 
   /*
     Function to allocate seats in same row
@@ -37,21 +39,24 @@ function App() {
   const allocateSeatsInSameRow = (rowIndex) => {
     const cloneMatrix = getCloneMatrix(seatMatrix);
     let seatsToBeBooked = userInput;
+    let seatNumbers = "";
     for (let j = 0; j < cloneMatrix[rowIndex].length; j++) {
       if (cloneMatrix[rowIndex][j] === VACANT_SEAT && seatsToBeBooked > 0) {
         cloneMatrix[rowIndex][j] = RESERVED_SEAT;
         seatsToBeBooked--;
         const seatNumber = String(rowIndex + 1) + String(j + 1);
-        setBookedSeats([...bookedSeats, seatNumber]);
-        console.log(bookedSeats)
+        seatNumbers += seatNumber + ",";
       }
     }
     setSeats(cloneMatrix);
     setReservedSeats(reservedSeats + parseInt(userInput));
     setVacantSeats(vacantSeats - parseInt(userInput));
-    cogoToast.success(
-      "Tickets booked successfully, Please note down your seat numbers"
-    );
+    setIsBooked(true);
+    setSeatsBooked(seatNumbers);
+    cogoToast.success("Tickets booked successfully", {
+      heading: "Success",
+      position: "top-right",
+    });
   };
 
   /*
@@ -59,6 +64,7 @@ function App() {
   */
   const allocateSeatsNearBy = () => {
     const cloneMatrix = getCloneMatrix(seatMatrix);
+    let seatNumbers = "";
     let seatsToBeBooked = userInput;
     for (let i = 0; i < cloneMatrix.length; i++) {
       for (let j = 0; j < cloneMatrix[i].length; j++) {
@@ -66,17 +72,19 @@ function App() {
           cloneMatrix[i][j] = RESERVED_SEAT;
           seatsToBeBooked--;
           const seatNumber = String(i + 1) + String(j + 1);
-          setBookedSeats([...bookedSeats, seatNumber]);
-          console.log(bookedSeats)
+          seatNumbers += seatNumber + ",";
         }
       }
     }
     setSeats(cloneMatrix);
     setReservedSeats(reservedSeats + parseInt(userInput));
     setVacantSeats(vacantSeats - parseInt(userInput));
-    cogoToast.success(
-      "Tickets booked successfully, Please check your seat numbers in the green box below"
-    );
+    setIsBooked(true);
+    setSeatsBooked(seatNumbers);
+    cogoToast.success("Tickets booked successfully", {
+      heading: "Success",
+      position: "top-right",
+    });
   };
 
   /*
@@ -98,25 +106,47 @@ function App() {
     Function to handle book seats when user clicks on the button
   */
   const handleBookSeats = () => {
-    if (isNaN(userInput)) {
-      cogoToast.error("Invalid Input : Your input should be a number");
+    if (isNaN(parseInt(userInput))) {
+      cogoToast.error("Invalid Input : Your input should be a number", {
+        heading: "Error",
+        position: "top-right",
+      });
       return;
     } else if (userInput <= 0) {
-      cogoToast.error("Invalid Input : You shoud book atleast 1 seat");
+      cogoToast.error("Invalid Input : You shoud book atleast 1 seat", {
+        heading: "Error",
+        position: "top-right",
+      });
       return;
     } else if (userInput > MAX_SEATS_ALLOWED_TO_BOOK) {
       cogoToast.error(
-        `Invalid Input : You can only book ${MAX_SEATS_ALLOWED_TO_BOOK} seats at a time`
+        `Invalid Input : You can only book ${MAX_SEATS_ALLOWED_TO_BOOK} seats at a time`,
+        {
+          heading: "Error",
+          position: "top-right",
+        }
       );
       return;
     } else if (vacantSeats - userInput < 0) {
       cogoToast.error(
-        `Only ${vacantSeats} seats are left, Please book your seats accordingly`
+        `Only ${vacantSeats} seats are left, Please book your seats accordingly`,
+        {
+          heading: "Error",
+          position: "top-right",
+        }
       );
       return;
     } else {
       return allocateSeats();
     }
+  };
+
+  /* 
+    Function to handle book seats again 
+  */
+  const handleBookAgain = () => {
+    setIsBooked(false);
+    setSeatsBooked("");
   };
 
   return (
@@ -126,10 +156,17 @@ function App() {
         reservedSeats={reservedSeats}
         vacantSeats={vacantSeats}
       />
-      <UserInput
-        userInput={userInput}
-        setUserInput={setUserInput}
-        handleBookSeats={handleBookSeats}
+      {!isBooked && (
+        <UserInput
+          userInput={userInput}
+          setUserInput={setUserInput}
+          handleBookSeats={handleBookSeats}
+        />
+      )}
+      <SeatsBooked
+        isBooked={isBooked}
+        seatsBooked={seatsBooked}
+        handleBookAgain={handleBookAgain}
       />
       <SeatMatrix seats={seats} />
     </div>
---
+++ src/components/SeatsBooked.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Alert, Button } from "react-bootstrap";
+import "../css/seatbooked.css";
+
+function SeatsBooked({ isBooked, seatsBooked, handleBookAgain }) {
+  const seatBookedArr = seatsBooked.split(",").filter((val) => val !== "");
+  return (
+    <div className="seat-booked-container">
+      {isBooked && (
+        <Alert variant="info">
+          <Alert.Heading>Your seat number(s) : </Alert.Heading>
+          <ul className="seat-numbers-list">
+            {seatBookedArr.map((seatNumber, index) => (
+              <li key={index}>{seatNumber}</li>
+            ))}
+          </ul>
+        </Alert>
+      )}
+      {isBooked && (
+        <Button variant="outline-success" onClick={handleBookAgain}>
+          Book Again
+        </Button>
+      )}
+    </div>
+  );
+}
+
+export default SeatsBooked;
